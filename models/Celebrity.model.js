ba537f8371fb12f2ae21df2feac4c09866c649ee
Emily Yang <emilyyang@Emilys-MacBook-Pro.local>
2024-11-16 14:44:48 +0100

    iteration 1,2, and 3 completed

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,24 @@
-//  Add your code here
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Define the Celebrity schema
+const celebSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Hey, put a name please!'] // Name is required
+  },
+  occupation: {
+    type: String,
+    default: "Unknown", // Default value if not provided
+  },
+  catchphrase: {
+    type: String,
+    required: [true, 'Where is the catch phrase???'] // Catchphrase is required
+  },
+});
+
+// Create the Celebrity model
+const Celeb = mongoose.model("Celeb", celebSchema);
+
+// Export the model
+module.exports = Celeb;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
+// linking movies 
+const movieRoutes = require ('./routes/movies.routes');
+// linking celebs 
+const celebRoutes = require ('./routes/celebrities.routes');
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
 
 module.exports = router;
